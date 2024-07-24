04fb70849a7aa84b067da15c626a186adac5fa32
Thomas Cansino <166280962+thomascansino@users.noreply.github.com>
2024-07-24 21:32:35 +0800

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# needs an input from you which will be stored in the $NAME variable
+read NAME
+
+# print a hello message followed by the $NAME variable
+echo Hello $NAME
+
+# print an error in the terminal
+bad_command
